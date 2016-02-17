c64e03c68f538c5d61b2f5b51ed0662dc14f3be0
devatha k <k.n.devs@gmail.com>
2016-02-17 16:30:05 -0500

    some typo

--- public/javascripts/entry.js
+++ public/javascripts/entry.js
@@ -4,7 +4,7 @@ var ReactDOM = require('react-dom');
 require("./dependency-one.js");
 
  
-// jsx not needed. it is replaced with babel. react tools not needed either
+// jsx not needed. it is replaced with babel. react tools not needed either which has jsx
 
 ReactDOM.render(
   <h1>Hello, from react!</h1>,

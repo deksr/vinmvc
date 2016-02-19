93c361b0f477891684529a366baa1d9d49e4385a
devatha k <k.n.devs@gmail.com>
2016-02-19 11:39:52 -0500

    states

--- public/javascripts/dependency-two.js
+++ public/javascripts/dependency-two.js
@@ -3,37 +3,42 @@ var ReactDOM = require('react-dom');
 
 
 // var name  needs start with caps
+// component starts here
 var Garden = React.createClass({ 
 
-	edit: function(){
-   alert ('editing note')
+// setting initial state of the dom 
+	getInitialState: function(){
+		return {
+			name: "Lilly of valley",
+			color: "white and green"
+		}
 	},
 
-	delete: function(){
-   alert ('deleting note')
+// add an event to set the new state
+	handleClick: function(){
+   this.setState({
+   	name: "Tulips"
+   })
 	},
 
+// render the elements
   render: function() {
     return (
-
-      <div className="flowers">
-      <p> {this.props.children} </p>
-      <span>
-	      <button onClick= {this.edit} className = "someclassname"> edit </button>
-	      <button onClick= {this.remove} className = "someclassname"> delete </button>
-      </span>
+      <div>
+      <button onClick={this.handleClick}> plant a bulb </button>
+      <br/>
+      You just now planted:  {this.state.name} in color {this.state.color}      
       </div>
     );
   }
 });
+// component ends here
 
 
 //need to always wrap them in div if you want multiple components to render
 ReactDOM.render( 
 	<div>
-		<Garden> lobella </Garden> 
-		<Garden> petunia </Garden>
-		<Garden> zenya </Garden> 
+		<Garden> </Garden>
   </div>
   , document.getElementById('content')
 );
